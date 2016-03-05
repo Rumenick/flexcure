e9fb74578cff423fd7bae0d11591f99335691f7d
Rumenick <rumenickbf@hotmail.com>
2016-03-05 02:52:35 -0300

    Correção de erro no arg. fixedpars (erro na função vcov).

--- R/curereg.R
+++ R/curereg.R
@@ -1,14 +1,3 @@
-############################################################################
-#
-# toInstall <- c("ggplot2", "reshape2", "RColorBrewer")
-# doInstall <- toInstall %in% rownames(installed.packages())
-# doInstall
-# if(doInstall){install.packages(toInstall, repos = "http://cran.us.r-project.org")}
-# lapply(toInstall, library, character.only = TRUE)
-
-# Just-in-time compilation
-compiler::enableJIT(3)
-
 ## Parameter transformations
 
 # logit function
@@ -32,7 +21,7 @@ loge <- function(x, bvalue=.Machine$double.eps, inverse = FALSE){
   return(eval(output))
 }
 
-# list of avaliable distribuition:
+## list of avaliable distribuition:
 # Standart Mixture - SM (sm)
 # Promotion Time - PT (pt)
 
@@ -288,47 +277,55 @@ curereg.dists <- list(
     }})
 )
 
-#  Função para ajuste de modelos com fração de cura:
+## Function for adjust regression for a flexible parametric survival models with cure fraction:
 
 curereg <- function(formula, cureformula=~1, data, weights, timedist = "moeev",
                     ncausedist = "poisson", subset, na.action, inits, method = "SANN", ...) {
 
-   call <- match.call()
-   argsfun <- list(...)
-   if (is.null(cureformula) || missing(cureformula)) {
-     dist <- timedist
-     argsfun$anc <- NULL
-     cureformula <- NULL
-     dcure <- NULL
-   }
-   else {
-     ncausedist <- match.arg(tolower(ncausedist), c("bernoulli", "poisson"))
-     dcure <- ifelse(ncausedist=="bernoulli", "sm", "pt")
-     dist <- match.arg(paste0(timedist, dcure), names(curereg.dists))
-     initp <- ifelse(missing(data),
-                     1-mean(get(all.vars(formula)[2])),
-                     ifelse(inherits(data, c("list", "data.frame")),
-                            1-mean(data[[all.vars(formula)[2]]]),
-                            stop("Must be data.frame or list")))
-     curereg.dists[[dist]]$inits <- curereg.dists[[dist]]$inits(initp=initp)
-     argsfun$anc <- list(theta=cureformula)
-     }
+  call <- match.call()
 
-  # Argumentos a serem passados para função flexsurvreg
+  # Arguments for flexsurvreg function:
+  argsfun <- list(...)
+  if (is.null(cureformula) || missing(cureformula)) {
+    dist <- timedist
+    argsfun$anc <- NULL
+    cureformula <- NULL
+    dcure <- NULL
+  }
+  else {
+    ncausedist <- match.arg(tolower(ncausedist), c("bernoulli", "poisson"))
+    dcure <- ifelse(ncausedist=="bernoulli", "sm", "pt")
+    dist <- match.arg(paste0(timedist, dcure), names(curereg.dists))
+    argsfun$anc <- list(theta=cureformula)
+    if (missing(inits)) {
+      argsfun$inits <- NULL
+      initp <- ifelse(missing(data),
+                      1-mean(get(all.vars(formula)[2])),
+                      ifelse(inherits(data, c("list", "data.frame")),
+                             1-mean(data[[all.vars(formula)[2]]]),
+                             stop("Must be data.frame or list")))
+      curereg.dists[[dist]]$inits <- curereg.dists[[dist]]$inits(initp=initp)
+    }
+    else {
+      argsfun$inits <- inits
+      }
+  }
+
   argsfun$control$maxit <- if (is.null(argsfun$control$maxit)) 10000 else argsfun$control$maxit
   argsfun$formula <- formula
   argsfun$data <- data
   argsfun$weights <- if(missing(weights)) NULL else weights
-  argsfun$inits <- if(missing(inits)) NULL else inits
   argsfun$dist <- if (dist %in% names(curereg.dists)) curereg.dists[[dist]]  else dist
   argsfun$method <- method
   argsfun$subset <- if(missing(subset)) NULL else subset
   argsfun$na.action <- if(missing(na.action)) NULL else na.action
 
-  # Ajustando modelo
+  # Fit model
+
   fit <- do.call(flexsurv::flexsurvreg, argsfun)
 
-  # Construindo saída
+  # Output
+
   out <- list(call = call)
   out$formula <- formula
   out$cureformula <- cureformula
@@ -371,13 +368,13 @@ curereg <- function(formula, cureformula=~1, data, weights, timedist = "moeev",
       namesdim <- names(out$std.error$se.time)
     }
   } else {
-         icure <- grep("theta",names.pars)
-          iloc <- grep(fit$dlist$location, names.pars)
-         itime <- which(names.pars %in% colnames(out$X))
-        iother <- seq_along(names.pars)[-c(icure, iloc, itime)]
-        ipartime <- c(iloc, itime, iother)
-        ipar <- c(icure, ipartime)
-        names(fit$coef) <- names.pars
+    icure <- grep("theta", names.pars)
+    iloc <- grep(fit$dlist$location, names.pars)
+    itime <- which(names.pars %in% colnames(out$X))
+    iother <- seq_along(names.pars)[-c(icure, iloc, itime)]
+    ipartime <- c(iloc, itime, iother)
+    ipar <- c(icure, ipartime)
+    names(fit$coef) <- names.pars
     out$coefficients <- list(coef.cure = fit$coef[icure], coef.time = fit$coef[ipartime])
     out$std.error <- list(se.cure = fit$res.t[icure,"se"], se.time = fit$res.t[ipartime, "se"])
     names(out$coefficients$coef.time)[1:(length(itime)+1)] <- colnames(out$X)
@@ -388,31 +385,37 @@ curereg <- function(formula, cureformula=~1, data, weights, timedist = "moeev",
     namesdim <- c(names(out$std.error$se.cure), names(out$std.error$se.time))
   }
 
+
   if (!is.matrix(fit$cov)) {
     out$vcov <- NA
-#   } else {
-#     if(!is.null(argsfun$fixedpars)){ Se fixedpars = TRUE
-#       aux <- numeric(length(argsfun$fixedpars))
-#       for(i in seq_along(argsfun$fixedpars)){ aux[i] <- which(argsfun$fixedpars[i] == ipar) }
-#       out$vcov <- fit$cov[ipar[-aux],ipar[-aux], drop=F]
-#       dimnames(out$vcov) <- list(namesdim[-aux], namesdim[-aux])
-     } else{
-      out$vcov <- fit$cov[ipar,ipar, drop=F]
+  } else {
+    if (!is.null(argsfun$fixedpars)) {
+      ntpars <- length(ipar)
+      out$vcov <- matrix(ncol = ntpars, nrow = ntpars)
+      out$vcov[-argsfun$fixedpars, -argsfun$fixedpars] <- fit$cov
+      aux <- numeric(length(argsfun$fixedpars))
+      for(i in seq_along(argsfun$fixedpars)){ aux[i] <- which(argsfun$fixedpars[i] == ipar) }
+      out$vcov <- out$vcov[ipar[-aux], ipar[-aux]]
+      dimnames(out$vcov) <- list(namesdim[-aux], namesdim[-aux])
+    } else {
+      out$vcov <- fit$cov[ipar,ipar, drop = F]
       dimnames(out$vcov) <- list(namesdim, namesdim)
     }
-#  }
+  }
 
   out$npars <- fit$npars
   out$loglik <- fit$loglik
   out$AIC <- fit$AIC
   out$data <- model.frame(fit)
   out$opt <- fit$opt
+  out$flexsurv <- fit
   rm(fit)
   out$ncausedist <- ncausedist
   out$timedist <- timedist
   class(out) <- "curereg"
   return(out)
 }
+
 
 print.curereg  <- function(x, ...) {
   cat("Call:\n")
@@ -496,20 +499,17 @@ summary.curereg  <- function(x, ...) {
   invisible(x)
 }
 
-# terms = time or cure or NULL (default)
+# terms = time or cure or missing (default)
+
 coef.curereg <- function(x, terms){
   if(missing(terms)) x$coefficients else x$coefficients[[paste0("coef.",terms)]]
 }
 
-# Usar flexsurv::model.frame.flexsurvreg
-predict.curereg <- function(x, newX, newZ, ...) {
-
-}
-
-# Predizer a fração de cudaros
+# Predict cure fraction
 
 curefraction <- function(x, newData = NULL, unique = TRUE, ordered = TRUE, n = 6){
-  curefun <- if(x$ncausedist == "poisson") function(x) exp(-exp(x)) else function(x) logit(x, inverse = T)
+  if(!inherits(x, "curereg")) stop("Object must be results of curereg")
+  curefun <- if(x$ncausedist == "poisson") function(x) exp(-exp(x)) else function(x) logit(x, inverse = TRUE)
   df <- if(is.null(newData)) x$Z else newData
   df <- if(unique) unique(df) else df
   cure <- curefun(tcrossprod(coef(x, terms = "cure"), df))[,]
@@ -522,13 +522,18 @@ curefraction <- function(x, newData = NULL, unique = TRUE, ordered = TRUE, n = 6
 }
 
 
-plot.curereg <- function(x, ...) {
+# predict.curereg <- function(x, newX, newZ, ...) {
+#
+# }
 
+
+plot.curereg <- function(x, ...) {
+  flexsurv::plot.flexsurvreg(x$flexsurv, ...)
 }
 
 
 lines.curereg <- function(x, ...) {
-
+  flexsurv::lines.flexsurvreg(x$flexsurv, ...)
 }
 
 logLik.curereg <- function(object, ...) {
